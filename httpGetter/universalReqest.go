49031b2c28750a457544d9ab56c9e6121d361434
Valentin Semenov <valentin.semyonov@nexign.com>
2023-12-07 19:45:12 +0300

    dasc

--- httpGetter/universalReqest.go
+++ httpGetter/universalReqest.go
@@ -27,7 +27,7 @@ type UniversalRequest struct {
 	Client resty.Client
 }
 
-// todo fe
+// todo fegg
 func NewRequestParams(url string, method string, respStatus int, headers []map[string]string, params []map[string]string, body interface{}) *RequestParams {
 	return &RequestParams{
 		Method:     method,
