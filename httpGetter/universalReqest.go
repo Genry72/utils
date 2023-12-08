85083e4b47b2f4ee0fc3de66299ed46de6ead1ac
Valentin Semenov <valentin.semyonov@nexign.com>
2023-12-08 03:03:16 +0300

    123

--- httpGetter/universalReqest.go
+++ httpGetter/universalReqest.go
@@ -27,7 +27,7 @@ type UniversalRequest struct {
 	Client resty.Client
 }
 
-// fdsfa
+//1,2
 
 func NewRequestParams(url string, method string, respStatus int, headers []map[string]string, params []map[string]string, body interface{}) *RequestParams {
 	return &RequestParams{
