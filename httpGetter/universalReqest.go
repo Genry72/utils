ac205c83cae94af766b4f590c45a3f1e2cc4bdc5
Valentin Semenov <valentin.semyonov@nexign.com>
2023-12-08 03:23:13 +0300

    3,4

--- httpGetter/universalReqest.go
+++ httpGetter/universalReqest.go
@@ -27,7 +27,7 @@ type UniversalRequest struct {
 	Client resty.Client
 }
 
-//1,2
+//1,2,3,4
 
 func NewRequestParams(url string, method string, respStatus int, headers []map[string]string, params []map[string]string, body interface{}) *RequestParams {
 	return &RequestParams{
